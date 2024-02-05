d5f176dbcbc35b201a68f6f713f2609470d45fc2
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2024-02-05 21:42:54 +0530

    uploading files to firebase storage

--- src/scenes/dashboard/dashboard.jsx
+++ src/scenes/dashboard/dashboard.jsx
@@ -1,26 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "../../firebase"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db, storage } from "../../firebase";
 
 const Dashboard = () => {
 
+    const [file, setFile] = useState(null);
+
     const insertItem = async() => {
         try {
-            const docRef = await addDoc(collection(db, "expenses"), {
-                title: 'fake title 1',
-                description: 'fake description 1',
-                expenseDate: '2023-12-23',
-                currentDate: serverTimestamp()
-            });
-            window.alert(`Document written with ID:  ${docRef.id}`);
+            if(!file){
+                window.alert("Plesae upload the image first!");
+            }else{
+                const fileName = new Date().getTime() + file.name;
+                const storageRef = ref(storage, fileName);
+                const uploadTask = uploadBytesResumable(storageRef, file);
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        switch (snapshot.state) {
+                            case 'paused':
+                                window.alert('Upload is paused');
+                                break;
+                        }
+                    }, 
+                    (error) => {
+                        switch (error.code) {
+                            case 'storage/unauthorized':
+                                break;
+                            case 'storage/canceled':
+                                break;
+                            case 'storage/unknown':
+                                break;
+                            default:
+                                break;
+                        }
+                    }, 
+                    () => {
+                        // Upload completed successfully, now we can get the download URL
+                        // if the img is uploaded successfully, other data is get saved in the db
+                        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+                            //get the img uploaded url and it save in the database
+                            const docRef = await addDoc(collection(db, "expenses"), {
+                                title: 'fake title 1',
+                                description: 'fake description 1',
+                                expenseDate: '2023-12-23',
+                                img: downloadURL,
+                                currentDate: serverTimestamp()
+                            });
+                            window.alert(`Document written with ID:  ${docRef.id}`);
+                        });
+                    }
+                );
+            }
         } catch (e) {
-            window.alert(`Error adding document:  e`);
+            window.alert(`Error adding document:  ${e}`);
         }
     };
 
     return(
         <div className="dashboard-page-wrapper">
             <div className="dasboard-page-pallete">
+                <label htmlFor="img-upload" className="img-upload-label">
+                    Select Image
+                    <input 
+                        id="img-upload" 
+                        className="img-upload" 
+                        name="img-upload" 
+                        type="file" 
+                        accept="image/png, image/jpg, image/gif, image/jpeg"
+                        onChange={(e) => {
+                            setFile(e.target.files[0]);
+                        }}
+                    />
+                </label>
                 <button onClick={() => {insertItem()}}>Insert Expense</button>
             </div>
         </div>
