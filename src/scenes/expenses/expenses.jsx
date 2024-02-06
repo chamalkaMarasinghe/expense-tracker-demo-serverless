c6e96eee4642a6111751338631198efc9402a2aa
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2024-02-06 13:52:22 +0530

    fetching all data within a collection at once

--- src/scenes/expenses/expenses.jsx
+++ src/scenes/expenses/expenses.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-import Img from '../../assets/images/login-background.jpg'
+import React, { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Expenses = () => {
+
+    const [expenseList, setExpenseList] = useState([]);
+    const [dataFetching, setDataFetching] = useState(true);
+
+    const loadData = async() => {
+        const querySnapshot = await getDocs(collection(db, "expenses"));
+        const data = [];
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            data.push(doc.data());
+        });
+        return data;
+    }
+
+    // handle the styling during the data fetching time
+    useEffect(() => {
+        if(dataFetching){
+            const items = document.getElementsByClassName("expense-item");
+            for (var i = 0; i < items.length; ++i) {
+                items[i].style.display='none';
+            }
+        }else{
+            document.getElementsByClassName('expenses-list')[0].style.backgroundImage='none';
+            const items = document.getElementsByClassName("expense-item");
+            for (var i = 0; i < items.length; ++i) {
+                items[i].style.display='flex';
+            }
+        }
+    }, [dataFetching]);
+
+    useEffect(() => {
+        loadData().then((data) => {setExpenseList(data); setDataFetching(false)});
+    }, [])
+
     return(
         <div className="expenses-page-wrapper">
             <div className="expenses-list-wrapper">
@@ -10,96 +45,27 @@ const Expenses = () => {
                     <hr />
                 </div>
                 <div className="expenses-list">
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee Boarding house rental fee Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner handover 10 thousand ruppes to the wardner handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="expense-item">
-                        <div className="col-1">
-                            <img src={Img}/>
-                        </div>
-                        <div className="col-2">
-                            <div className="title">Boarding house rental fee</div>
-                            <hr />
-                            <div className="date">2024-01-05</div>
-                            <div className="description">
-                                <p>
-                                    handover 10 thousand ruppes to the wardner
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        expenseList.map((expense, index) => {
+                            return(
+                                <div className="expense-item" key={index}>
+                                    <div className="col-1">
+                                        <img src={expense.img}/>
+                                    </div>
+                                    <div className="col-2">
+                                        <div className="title">{expense.title}</div>
+                                        <hr />
+                                        <div className="date">{expense.expenseDate}</div>
+                                        <div className="description">
+                                            <p>
+                                                {expense.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
